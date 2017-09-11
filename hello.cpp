89afebc098091a0410014404edcb9582cf723ea0
jassi-bhullar <jassi-bhullar@github.com>
2017-09-11 18:48:07 +0530

    Sort by Stack, only 2 stacks, pop push top only

--- hello.cpp
+++ hello.cpp
@@ -1,27 +1,7 @@
 // tail recusrion example 
 
 // void foo(int n, int running_total = 0)
-#include <bits/stdc++.h>
-using namespace std;
 
-int foo(){
+// ankit method for diameter of tree
 
-  for (int i=0; i<7; i++){
-    if(i==3) return i;
-    cout << i << " time\n";// endl;
-  }
-}
-
-int main(){
-  int i=0;
-  int a[] = {6,2,2,-23,5,16};
-  vector<int> s(a,a+6);
-
-  sort(s.begin(), s.end());
-
-  for(vector<int>::iterator itr=s.begin(); itr!=s.end(); ++itr)
-    cout << *itr << " ";
-  cout << endl;
-
-  return 0;
-}
+// sort stack using Rishab's way
